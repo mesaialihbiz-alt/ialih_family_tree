ba9438ba7fcebd4db88d5bf7fc06ec7009c5a3e7
mesaialihbiz-alt <mesaialihbiz@gmail.com>
2026-07-12 21:35:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html for root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Phawa Ialih Family Tree running on port ${PORT}`);
+});
