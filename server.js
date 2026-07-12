149d3e23d3751a1605ee9c9dea27331e30cd13f3
mesaialihbiz-alt <mesaialihbiz@gmail.com>
2026-07-12 22:26:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
+// Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
 
 // Serve index.html for root route
